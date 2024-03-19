abd2d78541f124a75397f78557e015a023706663
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-03-19 17:37:14 +0100

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,54 @@
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+const formObject = {};
 
+function loadFormData() {
+  const storedData = localStorage.getItem(localStorageKey);
+  if (storedData) {
+    const parsedData = JSON.parse(storedData);
+    if (parsedData) {
+      form.elements.email.value = parsedData.email;
+      form.elements.message.value = parsedData.message;
+      Object.assign(formObject, parsedData);
+    }
+  }
+}
+
+function saveFormData() {
+  const formData = new FormData(form);
+  formData.forEach((value, key) => {
+    formObject[key] = value.trim();
+  });
+  localStorage.setItem(localStorageKey, JSON.stringify(formObject));
+}
+
+function resetForm() {
+  form.reset();
+  localStorage.removeItem(localStorageKey);
+}
+
+form.addEventListener('input', () => {
+  const formData = new FormData(form);
+
+  formData.forEach((value, key) => {
+    formObject[key] = value.trim();
+  });
+
+  localStorage.setItem(localStorageKey, JSON.stringify(formObject));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = e.currentTarget.elements.email.value;
+  const message = e.currentTarget.elements.message.value;
+
+  if (email === '' || message === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(formObject);
+    resetForm();
+  }
+});
+
+loadFormData(); // Здесь вызываем функцию loadFormData() для загрузки данных при загрузке страницы
